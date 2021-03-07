e5a9b738827af1944f603442aff9abb04fd32dc4
isabelpaz1999 <44704905+isabelpaz1999@users.noreply.github.com>
2021-03-06 22:19:22 -0800

    Update Universe.java

--- src/others/Universe.java
+++ src/others/Universe.java
@@ -1,5 +1,33 @@
 package src;
 
 public class Universe {
+
+    private float age;
+    private float mass;
+    private float expandingRate;
+
+public float getAge(){
+    return age;
+}
+
+public void setAge(float age){
+    this.age = age;
+}
+
+public float getMass(){
+    return mass;
+}
+
+public void setMass(float mass){
+    this.mass = mass;
+}
+
+public float getExpandingRate(){
+    return expandingRate;
+}
+
+public void setExpandingRate(float expandingRate){
+    this.expandingRate = expandingRate;
+}
     
 }
