44b2cb0298882d50f0dea34edc36837542bc9795
natgoodrich371 <64048848+natgoodrich371@users.noreply.github.com>
2021-03-07 14:01:12 -0800

    Update Asteroid.java

--- src/celestialbody/Asteroid.java
+++ src/celestialbody/Asteroid.java
@@ -1,5 +1,34 @@
 package src.celestialbody;
 
 public class Asteroid {
+    private float orbitalPeriod;
+    private float orbitalSpeed;
+}
+
+public void burnUp() {
+
+}
+
+public void hitPlanet(String Planet) {
+
+}
+
+public void hitStar(String Star) {
     
+}
+
+public float getOrbitalPeriod() {
+    return orbitalPeriod;
+}
+
+public float getOrbitalSpeed() {
+    return orbitalSpeed;
+}
+
+public void setOrbitalPeriod(float orbitalPeriod) {
+    this.orbitalPeriod = orbitalPeriod;
+}
+
+pulic void setOrbitalSpeed(float orbitalSpeed) {
+    this.orbitalSpeed = orbitalSpeed;
 }
