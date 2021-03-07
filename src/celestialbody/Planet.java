bf25dec94ac592986c3141e9973899577c0f7a5d
ryanmercado6 <32846563+ryanmercado6@users.noreply.github.com>
2021-03-06 22:18:14 -0800

    Update Planet.java

--- src/celestialbody/Planet.java
+++ src/celestialbody/Planet.java
@@ -1,5 +1,32 @@
 package src.celestialbody;
 
 public class Planet {
-    
+    private int rings;
+    private float orbitalSpeed;
+    private float orbitalPeriod;
+    private float rotationPeriod;
+        public void setRings(float ringamt){
+        rings=ringamt;
+        }
+        public void setSpeed(float speed){
+        oribtalSpeed=speed;
+        }
+        public void setOPeriod(float Operiod){
+        orbitalPeriod=Operiod;
+        }
+        public void setRPeriod(float Rperiod){
+        rotationPeriod=Rperiod;
+        }
+        public int getRings(){
+        return rings;
+        }
+        public float getSpeed(){
+        return oribtalSpeed;
+        }
+        public float getOPeriod(){
+        return orbitalPeriod;
+        }
+        public float getRPeriod(){
+        return rotationPeriod;
+        }    
 }
